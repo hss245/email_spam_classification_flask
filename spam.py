8382a91e5d4c7ec9d9898e8bd7fa08c492be1a4c
Harpreet Singh Sachdev <49908818+hss245@users.noreply.github.com>
2019-09-25 13:42:24 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn import naive_bayes
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix,accuracy_score
